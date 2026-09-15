93b1520e3cd4c0a347fe66bebd09abc0979b2f84
Julie Kuramochi <julie.kuramochi@mindlinksoft.com>
2026-09-15 16:29:00 +0100

    Added tests for move rover forward spec. Updated Rover to a class, added CommandF method to Rover

---
+++ WakingSkeleton.Test/MarsRoverMoveForwardTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace WakingSkeleton.Test;
+
+[TestFixture]
+public class MarsRoverMoveForwardTests
+{
+    public static IEnumerable<TestCaseData> RoverMovingForwardTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(Direction.North, (1, 2));
+        }
+    }
+
+    [Test]
+    [TestCaseSource(nameof(RoverMovingForwardTestCases))]
+    public void RoverMovingForwardChangesPosition(Direction direction, (int, int) expectedPosition)
+    {
+        Rover rover = new Rover((1, 1), direction);
+
+        rover.CommandF();
+        
+        (int, int) actualPosition = rover.Position;
+        Assert.That(actualPosition, Is.EqualTo(expectedPosition));
+    }
+}
--- WakingSkeleton/Rover.cs
+++ WakingSkeleton/Rover.cs
@@ -1,3 +1,18 @@
 ﻿namespace WakingSkeleton;
 
-public sealed record Rover((int, int) Position, Direction Direction);
+public class Rover
+{
+    public Direction Direction { get; set; }
+    public (int, int) Position { get; set; }
+
+    public Rover((int, int) position, Direction direction)
+    {
+        Position = position;
+        Direction = direction;
+    }
+    
+    public void CommandF()
+    {
+        Position = Position with { Item2 = Position.Item2 + 1 };
+    }
+}
