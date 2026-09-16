8778bce85ed5982ce39e5190a2517a7078732ca4
Harry Coneboy <harry.coneboy@mindlinksoft.com>
2026-09-16 15:28:20 +0100

    AddItem test and created Order class

---
+++ WakingSkeleton.Test/AddItemTests.cs
@@ -0,0 +1,31 @@
+﻿namespace WakingSkeleton.Test;
+
+[TestFixture]
+internal class AddItemTests
+{
+    [Test]
+    public void AddItemWithSufficientStockToOrder()
+    {
+        int productId = 327;
+
+        Order order = new Order();
+
+        order.AddItem(productId);
+
+        int productQuantity = order.GetProductQuantity(productId);
+
+        Assert.That(productQuantity, Is.EqualTo(1));
+    }
+}
+
+internal class Order
+{
+    public int GetProductQuantity(int productId)
+    {
+        return 1;
+    }
+
+    public void AddItem(int productId)
+    {
+    }
+}
